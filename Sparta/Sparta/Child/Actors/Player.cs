54bce0df98afe77936c41c74b9dfd9d75959dec2
LJH-0131 <lyk84210@naver.com>
2025-04-23 17:20:41 +0900

    이정호 : 직업 이름 선택 오류 임시 개선

--- Sparta/Sparta/Child/Actors/Player.cs
+++ Sparta/Sparta/Child/Actors/Player.cs
@@ -57,16 +57,16 @@ namespace Sparta.Child.Actors
                 switch (selectedIndex)
                 {
                     case -1:
-                        break;
+                        continue;
                     case 1:
                         Console.WriteLine("원하시는 행동을 입력해주세요\n");
                         break;
                     case 2:
                         Console.Clear();
                         BeginPlay();
-                        break;
-                    default:
                         return;
+                    default:
+                        continue;
                 }
 
                 int jobselect;
@@ -77,43 +77,53 @@ namespace Sparta.Child.Actors
                     Console.WriteLine("직업을 선택해주세요.\n 1. 전사\n 2. 암살자\n 3. 탱커\n");
                     JobNum = Console.ReadLine();
 
-                if (JobNum != "1" && JobNum != "2" && JobNum != "3" && JobNum != "스파르타")
-                {
-                    Console.Clear();
-                    Console.WriteLine("존재하는 직업을 선택해주세요\n");
-                    continue;
-                }
-                else if (JobNum == "스파르타")
-                {
-                    Job = "스파르타";
-                }
-                else if (int.Parse(JobNum) == 1)
-                {
-                    Job = "전사";
-                }
-                else if (int.Parse(JobNum) == 2)
-                {
-                    Job = "암살자";
-                }
-                else if (int.Parse(JobNum) == 3)
-                {
-                    Job = "탱커";
-                }
-                else
-                {
-                    return;
-                }
-                while (true)
-                {
+                    if (JobNum != "1" && JobNum != "2" && JobNum != "3" && JobNum != "스파르타")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("존재하는 직업을 선택해주세요\n");
+                        continue;
+                    }
+                    else if (JobNum == "스파르타")
+                    {
+                        Job = "스파르타";
+                    }
+                    else if (int.Parse(JobNum) == 1)
+                    {
+                        Job = "전사";
+                    }
+                    else if (int.Parse(JobNum) == 2)
+                    {
+                        Job = "암살자";
+                    }
+                    else if (int.Parse(JobNum) == 3)
+                    {
+                        Job = "탱커";
+                    }
+                    else
+                    {
+                        return;
+                    }
+
                     Console.Clear();
                     Console.WriteLine($"입력하신 직업은 {Job}입니다.");
                     Console.WriteLine("직업을 선택하시겠습니까?");
                     Console.WriteLine("1. 저장");
                     Console.WriteLine("2. 취소\n");
 
-                    selectedIndex = selector.Select();
+                    if (int.TryParse(Console.ReadLine(), out jobselect) && jobselect == 1)
+                    {
+                        break;
+                    }
 
-                    switch (selectedIndex)
+                    Console.Clear();
+                    Console.WriteLine($"입력하신 직업은 {Job}입니다.");
+                    Console.WriteLine("직업을 선택하시겠습니까?");
+                    Console.WriteLine("1. 저장");
+                    Console.WriteLine("2. 취소\n");
+
+                    int JobOk = int.Parse(Console.ReadLine());
+
+                    switch (JobOk)
                     {
                         case 1:
                             Console.WriteLine("원하시는 행동을 입력해주세요\n");
@@ -137,7 +147,6 @@ namespace Sparta.Child.Actors
 
                 break;
                 Console.Clear();
-            }
 
                 if (Job == "전사")
                 {
